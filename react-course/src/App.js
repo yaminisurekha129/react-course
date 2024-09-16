ec973be487fac3bccebce2347c31d5236ddd6558
Yamini Srirama Surekha Gosula <yaminisurekha2002@gmail.com>
2024-09-16 11:04:49 +0530

    first commit

---
+++ react-course/src/App.js
@@ -0,0 +1,13 @@
+
+import React,{useState} from "react";
+
+function App() {
+  useState();
+  return (
+    <div className="App">
+      <h1>Hello Hooks</h1>
+    </div>
+  );
+}
+
+export default App;
